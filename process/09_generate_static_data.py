c743705ff0582fdfdcdb4a43c0f0ff8143566193
weichangli <changliwei1993@gmail.com>
2020-02-19 15:27:27 +0800

    refine_data

---
+++ process/09_generate_static_data.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+
+all_steam_year = pd.read_csv('/Users/charles/PycharmProjects/Games_Research/data/yearly_data_for_static_variable/merge_yearly_data.csv')
+res_id_name = pd.read_csv('/Users/charles/PycharmProjects/Games_Research/data/game_features(all_static_variables)/games-features.csv')
+id_name = pd.read_csv('/Users/charles/PycharmProjects/Games_Research/data/remain_games/remain_games_id_name_2018-12-18 to 2019-03-11(after_delete_not_in_game_features).csv')[['ID', 'Name']]
+sta_data_df = pd.DataFrame(
+    columns=['ID', 'Name', 'Release date','RequiredAge','Developer(s)', 'Publisher(s)', 'PlatformWindows', 'PlatformLinux',
+             'PlatformMac'])
+sta_data_df[['ID', 'Name']] = id_name
+
+for index, row in all_steam_year.iterrows():
+    result_match = sta_data_df.loc[sta_data_df['Name'] == row['Game']]
+    if result_match.shape[0] == 1:
+        current_index = result_match.index.tolist()[0]
+        sta_data_df.loc[current_index, ['Release date', 'Developer(s)', 'Publisher(s)']] = [
+            row['Release date'], row['Developer(s)'], row['Publisher(s)']]
+
+for index, row in res_id_name.iterrows():
+    result_match = sta_data_df.loc[sta_data_df['ID'] == row['ResponseID']]
+    if result_match.shape[0] == 1:
+        current_index = result_match.index.tolist()[0]
+        sta_data_df.loc[current_index, ['RequiredAge','PlatformWindows','PlatformLinux', 'PlatformMac']] = [
+            row['RequiredAge'],row['PlatformWindows'], row['PlatformLinux'], row['PlatformMac']]
+
+sta_data_df.to_csv('/Users/charles/PycharmProjects/Games_Research/data/biweekly(static_data)/static.csv')
+print('hello')
---
+++ process/10_refine_data.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+import time
+import re
+
+whole_single_df = pd.read_csv('/Users/charles/PycharmProjects/Games_Research/data/biweekly(singlesheet_instatic_plus_time_period)/singlesheet_instatic.csv')[['ID', 'Name', 'Time_period', 'Owners before', 'Owners after', 'Sales', 'Price', 'Max discount','Userscore (Metascore)']]
+whole_single_df['Userscore']=whole_single_df['Userscore (Metascore)'].str.split('(', 1).str[0]
+whole_single_df['Metascore']=whole_single_df['Userscore (Metascore)'].str.split('(', 1).str[1]
+whole_single_df['Metascore']=whole_single_df['Metascore'].str.split(')', 1).str[0]
+whole_single_df['Userscore']=whole_single_df['Userscore'].str.strip()
+whole_single_df['Metascore']=whole_single_df['Metascore'].str.strip()
+
+whole_single_df.to_csv('/Users/charles/PycharmProjects/Games_Research/data/biweekly(split_score)/' + 'singlesheet_instatic_plus_time_period_split_score.csv')
+
+
+static_data_df = pd.read_csv('/Users/charles/PycharmProjects/Games_Research/data/biweekly(static_data)/static.csv')
+print(static_data_df.info())
+
+major_company_file_path = '/Users/charles/PycharmProjects/Games_Research/data/major_company/'
+
+major_company = pd.read_csv(major_company_file_path + 'MajorCompany.csv')
+
+company_list=pd.concat(major_company.iloc[:,i] for i in range(1,major_company.shape[1]))
+company_list.index=pd.np.arange(len(company_list))
+company_list=company_list.dropna().unique().tolist()
+print(type(company_list))
+print(len(company_list))
+print(company_list)
+
+
+def string_finder(columns, words):
+    if any(word in field for field in columns for word in words):
+        return True
+    return False
+
+
+static_data_df['isMajorCompany'] = static_data_df[['Publisher(s)']].astype(str).apply(string_finder,
+                                                                                      words=company_list, axis=1)
+print(static_data_df.info())
+
+static_data_df[['ID', 'Name', 'Release date','RequiredAge', 'Developer(s)', 'Publisher(s)', 'PlatformWindows', 'PlatformLinux',
+                'PlatformMac', 'isMajorCompany']].to_csv('/Users/charles/PycharmProjects/Games_Research/data/biweekly(static_data)/static_plus_major_company.csv')
+
+
+
