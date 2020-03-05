6514bd4e74942c35099340118c085861c0e9bf1e
weichangli <changliwei1993@gmail.com>
2020-03-05 18:47:17 +0800

    finish

--- Condition_Filter/04_PriceInitial and PriceFinal NOT EQUAL to 0.py
+++ Condition_Filter/04_PriceInitial and PriceFinal NOT EQUAL to 0.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-game_features_file_path = '/Users/charles/PycharmProjects/Games_Research/Condition_Filter/games-features(major_company).csv'
+game_features_file_path = '/Users/charles/PycharmProjects/Games_Research/Condition_Filter/games-features(age>3).csv'
 game_features_df = pd.read_csv(game_features_file_path)
 print(game_features_df.info())
 game_features_df = game_features_df.loc[(game_features_df["PriceInitial"] != 0) & (game_features_df["PriceFinal"] != 0)]
