c658c8428c904418e470229420c318fe81c2621c
weichangli <changliwei1993@gmail.com>
2020-03-11 17:31:02 +0800

    reset_index

--- Financial Statement/sort_by_stock_code.py
+++ Financial Statement/sort_by_stock_code.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 df_stock_list = pd.read_csv('/Users/charles/PycharmProjects/Games_Research/Financial Statement/game_stock_code(edited).csv')
 df_stock_list = df_stock_list.sort_values(by='code', ascending=True)
+df_stock_list = df_stock_list.reset_index()
 df_stock_list[['code','name','c_name']].to_csv('/Users/charles/PycharmProjects/Games_Research/Financial Statement/game_stock_code(sorted).csv')
