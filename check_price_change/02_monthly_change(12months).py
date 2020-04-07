66f2584e3647198934e954a226bd8825b5b72f3d
weichangli <changliwei1993@gmail.com>
2020-04-07 23:07:42 +0800

    finish_all_data

---
+++ check_price_change/02_monthly_change(12months).py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+import copy
+
+remain_id_file_path = '/Users/charles/PycharmProjects/Games_Research/append_monthly_quarterly/final(12).csv'
+
+remain_id_df=pd.read_csv(remain_id_file_path)
+
+monthly_data_df=pd.read_csv('/Users/charles/PycharmProjects/Games_Research/Filter_Excel_From_HTML/OUTPUT_DATA/Calendar_monthly_csv/2016-01-01 to 2016-01-31 (monthly_data).csv',index_col=0)
+
+print(monthly_data_df.columns.values)
+monthly_data_features_df = pd.DataFrame(columns=monthly_data_df.columns.values)
+
+monthly_file_path = '/Users/charles/PycharmProjects/Games_Research/Filter_Excel_From_HTML/OUTPUT_DATA/Calendar_monthly_csv/'
+monthly_file_list = os.listdir(monthly_file_path)
+monthly_file_list.sort()
+monthly_file_list=monthly_file_list[28:40]
+
+for index,row in remain_id_df.iterrows():
+    id=row['ID']
+    print(id)
+    for i in range(len(monthly_file_list)):
+        month_data_df = pd.read_csv(monthly_file_path + monthly_file_list[i])
+        monthly_data_features_df=monthly_data_features_df.append(month_data_df[month_data_df['ID']==id], sort=True, ignore_index=True)
+
+monthly_data_features_df=monthly_data_features_df.drop(["Unnamed: 0"],axis=1)
+monthly_data_features_df=monthly_data_features_df.reset_index()
+monthly_data_features_df=monthly_data_features_df.drop(["index"],axis=1)
+
+monthly_data_features_df.to_csv('/Users/charles/PycharmProjects/Games_Research/check_price_change/merge_12_month.csv')
