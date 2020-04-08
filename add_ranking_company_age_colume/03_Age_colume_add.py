5567cff7c91305de8d21cab09ca582d9457a4ff9
weichangli <changliwei1993@gmail.com>
2020-04-08 21:04:44 +0800

    form_complete_data

--- add_ranking_company_age_colume/03_Age_colume_add.py
+++ add_ranking_company_age_colume/03_Age_colume_add.py
@@ -7,7 +7,6 @@ game_features_df = pd.read_csv(game_features_file_path)
 game_features_df = game_features_df.loc[(game_features_df["ReleaseDate"] != 'Coming Soon') & (
         game_features_df["ReleaseDate"] != 'Coming to home video Q1 2020')]
 
-
 print(game_features_df.info())
 
 
@@ -24,9 +23,12 @@ game_features_df['Value'] = game_features_df.apply(lambda row: type_filter(row['
 game_features_df = game_features_df[game_features_df['Value'] != '']
 print(game_features_df.info())
 
-start_day = datetime(2020, 3, 3)
+# start_day = datetime(2017, 4, 1)
+# start_day = datetime(2018,5, 1)
+start_day = datetime(2018,7, 1)
 game_features_df['days'] = start_day - pd.to_datetime(game_features_df['ReleaseDate'])
-game_features_df.to_csv('/Users/charles/PycharmProjects/Games_Research/add_ranking_company_age_colume/games-features(days).csv')
+game_features_df.to_csv(
+    '/Users/charles/PycharmProjects/Games_Research/add_ranking_company_age_colume/games-features(days).csv')
 
 game_features_df = pd.read_csv(
     '/Users/charles/PycharmProjects/Games_Research/add_ranking_company_age_colume/games-features(days).csv')
@@ -36,19 +38,30 @@ game_features_df['age'] = game_features_df['age'].str.strip()
 
 print(game_features_df.info())
 
-game_features_df['one'] = 91
+# game_features_df['one'] = 30.41
+game_features_df['one'] = 91.25
 game_features_df['age'] = game_features_df['age'].astype('int').div(game_features_df['one'])
 game_features_df['age'] = game_features_df['age'].astype('int') + 1
 
 game_features_df = game_features_df.drop(["days"], axis=1)
 game_features_df = game_features_df.drop(["one"], axis=1)
-game_features_df.to_csv('/Users/charles/PycharmProjects/Games_Research/add_ranking_company_age_colume/games-features(age).csv')
+game_features_df.to_csv(
+    '/Users/charles/PycharmProjects/Games_Research/add_ranking_company_age_colume/games-features(age).csv')
 
-#game_features_df = game_features_df[game_features_df['age'] >= 3]
+# game_features_df = game_features_df[game_features_df['age'] >= 3]
 
-game_features_df=game_features_df.drop(["Unnamed: 0.1.1"],axis=1)
-game_features_df=game_features_df.drop(["Unnamed: 0.1"],axis=1)
-game_features_df=game_features_df.drop(["Unnamed: 0"],axis=1)
-game_features_df=game_features_df.drop(["Value"],axis=1)
+game_features_df = game_features_df.drop(["Unnamed: 0.1.1.1"], axis=1)
+game_features_df = game_features_df.drop(["Unnamed: 0.1.1"], axis=1)
+game_features_df = game_features_df.drop(["Unnamed: 0.1"], axis=1)
+game_features_df = game_features_df.drop(["Unnamed: 0"], axis=1)
+game_features_df = game_features_df.drop(["Value"], axis=1)
 print(game_features_df.info())
-game_features_df.to_csv('/Users/charles/PycharmProjects/Games_Research/add_ranking_company_age_colume/games-features(age).csv')
+
+# game_features_df.to_csv(
+#     '/Users/charles/PycharmProjects/Games_Research/add_ranking_company_age_colume/games-features(age_month6).csv')
+
+# game_features_df.to_csv(
+#     '/Users/charles/PycharmProjects/Games_Research/add_ranking_company_age_colume/games-features(age_month12).csv')
+
+game_features_df.to_csv(
+    '/Users/charles/PycharmProjects/Games_Research/add_ranking_company_age_colume/games-features(age_quarter6).csv')
